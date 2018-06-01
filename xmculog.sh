8e529e25324e8092e1205db9ebaaf6bcd008c5f0
bluphy <bluphyxw@gmail.com>
2018-06-01 17:25:36 +0800

    Modify the log path

--- xmculog.sh
+++ xmculog.sh
@@ -4,7 +4,7 @@
 #check the serial device, default value is "ttyUSB0"
 ls /dev | grep ttyS
 McuUSBSerialDevice="ttyS2"
-
+mculogpath="/cygdrive/c/tbox_log_tools/log/mculog"
 
 stty sane
 stty -F /dev/$McuUSBSerialDevice $tbxttycfg 38400 cs8
@@ -12,7 +12,7 @@ stty -F /dev/$McuUSBSerialDevice $tbxttycfg 38400 cs8
 #saving mcu serial log
 tboxlogname=`date  +%Y%m%d_%H%M%S_mcu.log`
 echo $tboxlogname
-cd ../log/mculog
+cd $mculogpath
 cat /dev/$McuUSBSerialDevice | tee $tboxlogname
 
 
