71c85acdf6c59ff210563ebeb9df498f7afea58a
bluphy <bluphyxw@gmail.com>
2018-06-01 10:37:47 +0800

    delete the timestamp to enhance the performance

--- xmculog.sh
+++ xmculog.sh
@@ -23,13 +23,5 @@ stty -F /dev/$McuUSBSerialDevice $tbxttycfg 38400 cs8
 tboxlogname=`date  +%Y%m%d_%H%M%S_mcu.log`
 echo $tboxlogname
 cd ../log/mculog
-cat /dev/$McuUSBSerialDevice | while read line
-do
-	nanosec=`date +%N`
-	sec=`date +"%Y%m%d %H:%M:%S"`
-	millisec=${nanosec:0:3} #date命令输出的纳秒会默认补齐长度，所以直接取纳秒的前三位
-
-	#echo "[$sec $millisec ] $line" >> $tboxlogname
-	echo "[$sec $millisec ] $line"
-done
+cat /dev/$McuUSBSerialDevice >> $tboxlogname
 
